7424d58a4eefc4bdfd9632f21de6601348e4ace0
unknown <bencbchurchill@gmail.com>
2020-03-28 16:36:31 -0700

    Configured Card to ignore unknown fields during deserialization

--- src/main/java/skaro/pokeflex/objects/card/Card_.java
+++ src/main/java/skaro/pokeflex/objects/card/Card_.java
@@ -2,10 +2,12 @@ package skaro.pokeflex.objects.card;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
 	"id",
